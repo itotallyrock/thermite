ee3f225b4c42f7b042e6ec164f0896979fdbb52f
Jeffrey Meyer <jam93t@mst.edu>
2022-10-19 00:06:49 -0500

    List out planned core chess types in lib.rs

--- thermite_core/src/lib.rs
+++ thermite_core/src/lib.rs
@@ -6,9 +6,12 @@ pub fn add(left: usize, right: usize) -> usize {
 mod tests {
     use super::*;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+//! Thermite chess essential types.
+//! - `Piece` - A piece on the board: King, Queen, Rook, Bishop, Knight, Pawn
+//! - `Side` - A player in the game, represented by the color of their pieces
+//! - `Square` - A single tile on a board, used for move notation
+//! - `Bitboard` - A mask of the board where each square can be either a `1` or a `0`
+//! - `ChessMove` - A from & to square for a piece and metadata necessary for making the move (eg. promotions)
+//! - `CastleRights` - The availability for a side to castle, keeps track of rook or king movement, and should also help move generation keep track of attacked squares
+//! - `Board` - The position: piece-arrangement, or where each piece is placed on the board, and side to move along with a myriad of featured gated metadata, for move-generation, evaluation, searching, and more.
+//! - `Score` - With `#[cfg(feature = "score")]` an evaluation of a [`Board`](crate::board::Board)
