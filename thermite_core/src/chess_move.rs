ed0ae1a79fd91439427c84e172c6fb74132b9cfe
Jeffrey Meyer <jam93t@mst.edu>
2022-12-25 02:26:53 -0600

    Implement Display for ChessMove

--- thermite_core/src/chess_move.rs
+++ thermite_core/src/chess_move.rs
@@ -1,4 +1,6 @@
+use std::fmt::{Display, Formatter};
 use crate::move_type::MoveType;
+use crate::piece_type::PieceType;
 use crate::square::Square;
 
 /// The move type and from/to squares for a chess move
@@ -10,4 +12,19 @@ pub struct ChessMove {
     pub from: Square,
     /// The ending square for the piece being moved
     pub to: Square,
+}
+
+impl Display for ChessMove {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        let &Self { from, to, move_type } = self;
+        match move_type {
+            MoveType::Quiet { .. } | MoveType::DoublePawnPush { .. } | MoveType::Capture { .. } | MoveType::EnPassantCapture { .. } | MoveType::KingSideCastle { .. } | MoveType::QueenSideCastle { .. } => {
+                write!(f, "{from}{to}")
+            },
+            MoveType::PromotingCapture { promotion, .. } | MoveType::Promotion { promotion } => {
+                let promotion = PieceType::from(promotion).get_lower_char();
+                write!(f, "{from}{to}{promotion}")
+            },
+        }
+    }
 }
