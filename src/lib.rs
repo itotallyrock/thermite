c2533dd285786213e5422c24fd7034d2aa1d2f05
Jeffrey Meyer <jam93t@mst.edu>
2023-04-23 23:42:46 -0500

    Make pieces.rs

--- src/lib.rs
+++ src/lib.rs
@@ -5,42 +5,13 @@ use arrayvec::ArrayVec;
 use nutype::nutype;
 use enum_map::{Enum, EnumMap};
 use derive_more::{AsMut, AsRef};
-use subenum::subenum;
 use bitmask_enum::bitmask;
 use player_color::PlayerColor;
 use square::Square;
 
 mod player_color;
 mod square;
-
-#[subenum(NonKingPieceType, NonPawnPieceType, PromotablePieceType)]
-#[derive(Enum, Copy, Clone, Eq, PartialEq, Debug, Ord, PartialOrd, Hash)]
-pub enum PieceType {
-    #[subenum(NonKingPieceType)]
-    Pawn,
-    #[subenum(NonKingPieceType, NonPawnPieceType, PromotablePieceType)]
-    Knight,
-    #[subenum(NonKingPieceType, NonPawnPieceType, PromotablePieceType)]
-    Bishop,
-    #[subenum(NonKingPieceType, NonPawnPieceType, PromotablePieceType)]
-    Rook,
-    #[subenum(NonKingPieceType, NonPawnPieceType, PromotablePieceType)]
-    Queen,
-    #[subenum(NonPawnPieceType)]
-    King,
-}
-
-#[derive(Copy, Clone, Eq, PartialEq, Debug, Hash)]
-pub struct OwnedPiece {
-    piece: PieceType,
-    player: PlayerColor,
-}
-
-#[derive(Copy, Clone, Eq, PartialEq, Debug, Hash)]
-pub struct PlacedPiece {
-    owned_piece: OwnedPiece,
-    square: Square,
-}
+mod pieces;
 
 #[derive(Copy, Clone, Eq, PartialEq, Debug, Default, Hash)]
 pub struct BoardMask(u64);
