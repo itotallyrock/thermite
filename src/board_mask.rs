ff0d27078198bd13465580005914a1be6685351d
Jeffrey Meyer <jam93t@mst.edu>
2023-04-25 23:33:46 -0500

    Make BoardMask nutype

--- src/board_mask.rs
+++ src/board_mask.rs
@@ -1,2 +1,11 @@
-#[derive(Copy, Clone, Eq, PartialEq, Debug, Default, Hash)]
+use nutype::nutype;
+
+#[nutype]
+#[derive(Copy, Clone, Eq, PartialEq, Debug, Hash)]
 pub struct BoardMask(u64);
+
+impl Default for BoardMask {
+    fn default() -> Self {
+        Self::new(0)
+    }
+}
