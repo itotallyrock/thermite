7f9ac5f8b6d2e1505108077f75efb58ce10589fe
Jeffrey Meyer <jam93t@mst.edu>
2023-04-30 01:54:39 -0500

    Test half_move_clock.rs

--- src/half_move_clock.rs
+++ src/half_move_clock.rs
@@ -19,8 +19,14 @@ impl HalfMoveClock {
 }
 
 impl HalfMoveClock {
-    pub fn increment(&mut self) {
-        *self = Self::new(PlyCount::new((*self.as_ref().as_ref()).saturating_add(1))).unwrap();
+    pub fn increment(&mut self) -> Result<(), InvalidHalfMoveClock> {
+        #[allow(clippy::cast_possible_truncation)]
+        if self.0 < PlyCount::new(HALF_MOVE_LIMIT_USIZE as u8) {
+            self.0.increment();
+            Ok(())
+        } else {
+            Err(InvalidHalfMoveClock)
+        }
     }
 
     pub fn reset(&mut self) {
@@ -29,3 +35,57 @@ impl HalfMoveClock {
 }
 
 pub const HALF_MOVE_LIMIT_USIZE: usize = 50;
+
+#[cfg(test)]
+mod test {
+    use crate::half_move_clock::{HalfMoveClock, InvalidHalfMoveClock};
+    use crate::ply_count::PlyCount;
+    use test_case::test_case;
+
+    #[test_case(0, Ok(0))]
+    #[test_case(1, Ok(1))]
+    #[test_case(2, Ok(2))]
+    #[test_case(3, Ok(3))]
+    #[test_case(4, Ok(4))]
+    #[test_case(5, Ok(5))]
+    #[test_case(6, Ok(6))]
+    #[test_case(7, Ok(7))]
+    #[test_case(8, Ok(8))]
+    #[test_case(9, Ok(9))]
+    #[test_case(10, Ok(10))]
+    #[test_case(25, Ok(25))]
+    #[test_case(50, Ok(50))]
+    #[test_case(51, Err(InvalidHalfMoveClock))]
+    #[test_case(120, Err(InvalidHalfMoveClock))]
+    #[test_case(250, Err(InvalidHalfMoveClock))]
+    fn new_works(input: u8, expected: Result<u8, InvalidHalfMoveClock>) {
+        let input = PlyCount::new(input);
+        let expected = expected.map(|n| HalfMoveClock::new(PlyCount::new(n)).expect("invalid test setup"));
+        assert_eq!(HalfMoveClock::new(input), expected);
+    }
+
+    #[test_case(0, Ok(1))]
+    #[test_case(1, Ok(2))]
+    #[test_case(2, Ok(3))]
+    #[test_case(3, Ok(4))]
+    #[test_case(4, Ok(5))]
+    #[test_case(5, Ok(6))]
+    #[test_case(6, Ok(7))]
+    #[test_case(7, Ok(8))]
+    #[test_case(8, Ok(9))]
+    #[test_case(49, Ok(50))]
+    #[test_case(50, Err(InvalidHalfMoveClock))]
+    fn increment_works(input: u8, expected: Result<u8, InvalidHalfMoveClock>) {
+        let mut input = HalfMoveClock::new(PlyCount::new(input)).expect("invalid test input");
+        let expected = expected.map(|n| HalfMoveClock::new(PlyCount::new(n)).expect("invalid test setup"));
+        assert_eq!(input.increment().map(|_| input), expected);
+    }
+
+    #[test]
+    fn reset_works() {
+        let mut input = HalfMoveClock::new(PlyCount::new(30)).expect("invalid test setup");
+        let expected = HalfMoveClock::new(PlyCount::new(0)).expect("invalid test setup");
+        input.reset();
+        assert_eq!(input, expected);
+    }
+}
